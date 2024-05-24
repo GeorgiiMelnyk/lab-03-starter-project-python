6580114aeeaf1006a26c7dc0f03d91a24ace5d98
GeorgiiMelnyk <georg999mel@gmail.com>
2024-05-24 14:56:48 +0300

    Add matrix route

--- spaceship/app.py
+++ spaceship/app.py
@@ -4,7 +4,7 @@ from starlette.responses import FileResponse
 
 from spaceship.config import Settings
 from spaceship.routers import api, health
-
+import numpy as np
 
 def make_app(settings: Settings) -> FastAPI:
     app = FastAPI(
@@ -24,5 +24,20 @@ def make_app(settings: Settings) -> FastAPI:
     @app.get('/', include_in_schema=False, response_class=FileResponse)
     async def root() -> str:
         return 'build/index.html'
+    
+    @app.get("/np")
+    def np_endpoint():
+        matrix_a = np.random.rand(10, 10)
+        matrix_b = np.random.rand(10, 10)
+
+        product = np.matmul(matrix_a, matrix_b)
+
+        result = {
+            "matrix_a": matrix_a.tolist(),
+            "matrix_b": matrix_b.tolist(),
+            "product": product.tolist(),
+        }
+
+        return result
 
     return app
